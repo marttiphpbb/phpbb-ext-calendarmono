c450f2dc104ec45c6666ef4c16606737678312f6
martti <info@martti.be>
2019-01-19 23:41:59 +0100

    listener for calendar forum events

--- event/view_listener.php
+++ event/view_listener.php
@@ -27,6 +27,8 @@ class view_listener implements EventSubscriberInterface
 		return [
 			'marttiphpbb.calendar.view'
 				=> 'marttiphpbb_calendar_view',
+			'marttiphpbb.calendar.view_forum'
+				=> 'marttiphpbb_calendar_view_forum',
 		];
 	}
 
@@ -36,8 +38,22 @@ class view_listener implements EventSubscriberInterface
 		$end_jd = $event['end_jd'];
 		$events = $event['events'];
 
-		$events = array_merge($events, $this->repo->get_all_for_period($start_jd, $end_jd));
+		$events = array_merge($events,
+			$this->repo->get_for_period($start_jd, $end_jd));
 
 		$event['events'] = $events;
- 	}
+	 }
+
+	 public function marttiphpbb_calendar_view_forum(event $event)
+	 {
+		 $start_jd = $event['start_jd'];
+		 $end_jd = $event['end_jd'];
+		 $forum_id = $event['forum_id'];
+		 $events = $event['events'];
+
+		 $events = array_merge($events,
+		 	$this->repo->get_for_period_and_forum($start_jd, $end_jd, $forum_id));
+
+		 $event['events'] = $events;
+	  }
 }
--- service/repo.php
+++ service/repo.php
@@ -20,13 +20,15 @@ class repo
 	protected $auth;
 	protected $user;
 	protected $topics_table;
+	protected $forums_table;
 
 	public function __construct(
 		db $db,
 		content_visibility $content_visibility,
 		auth $auth,
 		user $user,
-		string $topics_table
+		string $topics_table,
+		string $forums_table
 	)
 	{
 		$this->db = $db;
@@ -34,13 +36,54 @@ class repo
 		$this->auth = $auth;
 		$this->user = $user;
 		$this->topics_table = $topics_table;
+		$this->forums_table = $forums_table;
 	}
 
-	public function get_all_for_period(int $start_jd, int $end_jd):array
+	public function get_for_period(int $start_jd, int $end_jd):array
+	{
+		$forum_ids = array_keys($this->auth->acl_getf('f_read', true));
+
+		return $this->get_for_period_and_forum_ids($start_jd, $end_jd, $forum_ids);
+	}
+
+	public function get_for_period_and_forum(
+		int $start_jd,
+		int $end_jd,
+		int $forum_id
+	):array
+	{
+		$forum_ids = [];
+		$forum_acl = $this->auth->acl_getf('f_read', true);
+
+		$sql = 'select f2.forum_id
+			from ' . $this->forums_table . ' f1, ' .
+				$this->forums_table . ' f2
+			where f1.forum_id = ' . $forum_id . '
+				and f1.left_id <= f2.left_id
+				and f2.right_id >= f1.right_id';
+
+		$result = $this->db->sql_query($sql);
+
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			if (isset($forum_acl[$row['forum_id']]))
+			{
+				$forum_ids[] = $row['forum_id'];
+			}
+		}
+
+		$this->db->sql_freeresult($result);
+
+		return $this->get_for_period_and_forum_ids($start_jd, $end_jd, $forum_ids);
+	}
+
+	public function get_for_period_and_forum_ids(
+		int $start_jd,
+		int $end_jd,
+		array $forum_ids
+	):array
 	{
 		$events = [];
-
-		$forum_ids = array_keys($this->auth->acl_getf('f_read', true));
 
 		$sql = 'select t.topic_id, t.forum_id, t.topic_title,
 			t.' . cnst::COLUMN_START . ' as start_jd, t.' . cnst::COLUMN_END . ' as end_jd
